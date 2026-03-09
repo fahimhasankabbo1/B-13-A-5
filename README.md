2ab3ee3a45d87cac18a14384b01692ccf1bd93ed
fahimhasankabbo1 <fahimhasankabbo1@gmail.com>
2026-03-09 16:37:36 +0600

    add README answers

---
+++ README.md
@@ -0,0 +1,20 @@
+# Assignment 05 - GitHub Issues Tracker
+
+## 1. Difference between var, let, and const
+var: Function scoped, can be redeclared and updated.  
+let: Block scoped, can be updated but not redeclared.  
+const: Block scoped, cannot be updated or redeclared.
+
+## 2. What is the spread operator (...)?
+The spread operator (...) is used to expand elements of an array or object.
+
+## 3. Difference between map(), filter(), and forEach()
+map(): Returns a new array after modifying each element.  
+filter(): Returns a new array based on a condition.  
+forEach(): Loops through an array but does not return a new array.
+
+## 4. What is an arrow function?
+An arrow function is a shorter way to write functions in JavaScript using =>.
+
+## 5. What are template literals?
+Template literals use backticks (` `) to create strings and insert variables easily.
--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let allIssuesData = []
 
-fetch("issues.json")
-    .then(res => res.json())
-    .then(data => {
+fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+.then(res => res.json())
+.then(data => {
 
-        allIssuesData = data.data
+allIssuesData = data.data
 
-        renderIssues(allIssuesData)
+renderIssues(allIssuesData)
 
-    })
+})
+
 
 
 
@@ -16,65 +17,67 @@ const loginForm = document.getElementById("loginForm")
 
 if (loginForm) {
 
-    loginForm.addEventListener("submit", function (e) {
+loginForm.addEventListener("submit", function(e){
 
-        e.preventDefault()
+e.preventDefault()
 
-        const user = document.getElementById("username").value
-        const pass = document.getElementById("password").value
+const user = document.getElementById("username").value
+const pass = document.getElementById("password").value
 
-        const userError = document.getElementById("userError")
-        const passError = document.getElementById("passError")
-        const successMsg = document.getElementById("successMsg")
+const userError = document.getElementById("userError")
+const passError = document.getElementById("passError")
+const successMsg = document.getElementById("successMsg")
 
-        userError.innerText = ""
-        passError.innerText = ""
-        successMsg.innerText = ""
+userError.innerText = ""
+passError.innerText = ""
+successMsg.innerText = ""
 
-        if (user !== "admin") {
-            userError.innerText = "Username incorrect"
-        }
+if(user !== "admin"){
+userError.innerText = "Username incorrect"
+}
 
-        if (pass !== "admin123") {
-            passError.innerText = "Password incorrect"
-        }
+if(pass !== "admin123"){
+passError.innerText = "Password incorrect"
+}
 
-        if (user === "admin" && pass === "admin123") {
+if(user === "admin" && pass === "admin123"){
 
-            successMsg.innerText = "Login successful"
+successMsg.innerText = "Login successful"
 
-            setTimeout(function () {
-                window.location.href = "dashboard.html"
-            }, 1000)
+setTimeout(function(){
 
-        }
+window.location.href = "dashboard.html"
 
-    })
+},1000)
+
+}
+
+})
 
 }
 
 
 
-function renderIssues(data) {
+function renderIssues(data){
 
-    const grid = document.getElementById("issueGrid")
-    const count = document.getElementById("issueCount")
+const grid = document.getElementById("issueGrid")
+const count = document.getElementById("issueCount")
 
-    if (!grid) return
+if(!grid) return
 
-    grid.innerHTML = ""
+grid.innerHTML = ""
 
-    count.innerText = data.length + " Issues"
-
-
-    data.forEach(item => {
-
-        const border = item.status === "open"
-            ? "border-t-green-500"
-            : "border-t-purple-500"
+count.innerText = data.length + " Issues"
 
 
-        const card = `
+data.forEach(item => {
+
+const border = item.status === "open"
+? "border-t-green-500"
+: "border-t-purple-500"
+
+
+const card = `
 
 <div onclick="openModal(${item.id})"
 class="border border-t-4 ${border} p-4 rounded-lg shadow-sm bg-white cursor-pointer">
@@ -82,15 +85,15 @@ class="border border-t-4 ${border} p-4 rounded-lg shadow-sm bg-white cursor-poin
 <div class="flex justify-between mb-2">
 
 <img src="${
-    item.status === "open"
-        ? "icons/Open-Status.png"
-        : "icons/Closed- Status.png"
+item.status === "open"
+? "icons/Open-Status.png"
+: "icons/Closed- Status.png"
 }" class="w-4 h-4">
 
 <span class="text-[10px] font-bold px-2 py-0.5 rounded
 ${item.priority === "high" ? "bg-red-100 text-red-600"
-        : item.priority === "medium" ? "bg-yellow-100 text-yellow-700"
-        : "bg-gray-200 text-gray-600"}">
+: item.priority === "medium" ? "bg-yellow-100 text-yellow-700"
+: "bg-gray-200 text-gray-600"}">
 
 ${item.priority}
 
@@ -110,25 +113,35 @@ ${item.description}
 
 <div class="flex gap-2 mb-3">
 
-${
-            item.labels && item.labels.includes("enhancement")
+${item.labels.map(label => {
 
-                ?
+let color = ""
 
-                `<span class="text-xs font-bold bg-green-100 text-green-600 px-2 py-0.5 rounded">
-ENHANCEMENT
+if(label === "bug"){
+color = "bg-red-100 text-red-600"
+}
+
+else if(label === "help wanted"){
+color = "bg-yellow-100 text-yellow-700"
+}
+
+else if(label === "enhancement"){
+color = "bg-green-100 text-green-600"
+}
+
+else if(label === "documentation"){
+color = "bg-orange-100 text-orange-600"
+}
+
+else if(label === "good first issue"){
+color = "bg-blue-100 text-blue-600"
+}
+
+return `<span class="text-xs font-bold ${color} px-2 py-0.5 rounded">
+${label.toUpperCase()}
 </span>`
 
-                :
-
-                `<span class="text-xs font-bold bg-red-100 text-red-600 px-2 py-0.5 rounded">
-BUG
-</span>
-
-<span class="text-xs font-bold bg-yellow-100 text-yellow-700 px-2 py-0.5 rounded">
-HELP WANTED
-</span>`
-        }
+}).join("")}
 
 </div>
 
@@ -136,7 +149,7 @@ HELP WANTED
 <div class="text-[10px] text-gray-400 border-t pt-2">
 
 #${item.id} by ${item.author}<br>
-${item.createdAt}
+${new Date(item.createdAt).toLocaleDateString()}
 
 </div>
 
@@ -144,181 +157,203 @@ ${item.createdAt}
 
 `
 
-        grid.innerHTML += card
+grid.innerHTML += card
 
-    })
-
-}
-
-
-
-function changeTab(status) {
-
-    document.querySelectorAll(".tab-btn")
-        .forEach(btn => btn.classList.remove("active"))
-
-    document.getElementById("tab-" + status)
-        .classList.add("active")
-
-    const loader = document.getElementById("loader")
-    const grid = document.getElementById("issueGrid")
-
-    grid.innerHTML = ""
-    loader.classList.remove("hidden")
-
-    setTimeout(function () {
-
-        loader.classList.add("hidden")
-
-        if (status === "all") {
-
-            renderIssues(allIssuesData)
-
-        } else {
-
-            const filtered =
-                allIssuesData.filter(i => i.status === status)
-
-            renderIssues(filtered)
-
-        }
-
-    }, 600)
+})
 
 }
 
 
 
-function openModal(id) {
-
-    const issue = allIssuesData.find(i => i.id === id)
-
-    document.getElementById("m-title").innerText = issue.title
-
-    const status = document.getElementById("m-status")
-
-    if (issue.status === "open") {
-
-        status.innerText = "Opened"
-
-        status.className =
-            "bg-green-100 text-green-600 px-2 py-1 rounded text-xs font-bold"
-
-    }
-
-    else {
-
-        status.innerText = "Closed"
-
-        status.className =
-            "bg-purple-100 text-purple-600 px-2 py-1 rounded text-xs font-bold"
-
-    }
-
-    document.getElementById("m-meta").innerText =
-        "Opened by Fahim Ahmed • 22/02/2026"
-
-    document.getElementById("m-desc").innerText =
-        issue.description
-
-    document.getElementById("m-assignee").innerText =
-        "Fahim Ahmed"
-
-    document.getElementById("m-priority").innerText =
-        issue.priority
 
 
-    const labels = document.getElementById("m-labels")
+function changeTab(status){
 
-    labels.innerHTML = ""
+document.querySelectorAll(".tab-btn")
+.forEach(btn => btn.classList.remove("active"))
 
-    if (issue.label === "ENHANCEMENT") {
+document.getElementById("tab-"+status)
+.classList.add("active")
 
-        labels.innerHTML =
-            '<span class="bg-green-100 text-green-600 px-2 py-1 rounded text-xs font-bold">ENHANCEMENT</span>'
+const loader = document.getElementById("loader")
+const grid = document.getElementById("issueGrid")
 
-    }
+grid.innerHTML = ""
 
-    else {
+loader.classList.remove("hidden")
 
-        labels.innerHTML =
+setTimeout(function(){
 
-            '<span class="bg-red-100 text-red-600 px-2 py-1 rounded text-xs font-bold">BUG</span>\
-<span class="bg-yellow-100 text-yellow-700 px-2 py-1 rounded text-xs font-bold">HELP WANTED</span>'
+loader.classList.add("hidden")
 
-    }
+if(status === "all"){
 
+renderIssues(allIssuesData)
 
-    const modal = document.getElementById("issueModal")
+}
 
-    modal.classList.remove("hidden")
-    modal.classList.add("flex")
+else{
+
+const filtered =
+allIssuesData.filter(issue => issue.status === status)
+
+renderIssues(filtered)
+
+}
+
+},600)
 
 }
 
 
 
-function closeModal() {
 
-    const modal = document.getElementById("issueModal")
+function openModal(id){
 
-    modal.classList.add("hidden")
-    modal.classList.remove("flex")
+fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+.then(res => res.json())
+.then(data => {
+
+const issue = data.data
+
+document.getElementById("m-title").innerText = issue.title
+
+const status = document.getElementById("m-status")
+
+if(issue.status === "open"){
+
+status.innerText = "Opened"
+
+status.className =
+"bg-green-100 text-green-600 px-2 py-1 rounded text-xs font-bold"
+
+}
+
+else{
+
+status.innerText = "Closed"
+
+status.className =
+"bg-purple-100 text-purple-600 px-2 py-1 rounded text-xs font-bold"
+
+}
+
+document.getElementById("m-meta").innerText =
+`Opened by ${issue.author}`
+
+document.getElementById("m-desc").innerText =
+issue.description
+
+
+document.getElementById("m-assignee").innerText =
+issue.assignee || "Not Assigned"
+
+
+const priorityText = document.getElementById("m-priority")
+
+priorityText.innerText = issue.priority.toUpperCase()
+
+
+if(issue.priority === "high"){
+priorityText.className = "px-3 py-1 rounded-full text-xs font-bold bg-red-100 text-red-600"
+}
+
+else if(issue.priority === "medium"){
+priorityText.className = "px-3 py-1 rounded-full text-xs font-bold bg-yellow-100 text-yellow-700"
+}
+
+else{
+priorityText.className = "px-3 py-1 rounded-full text-xs font-bold bg-gray-100 text-gray-600"
+}
+
+
+const labels = document.getElementById("m-labels")
+
+labels.innerHTML = ""
+
+issue.labels.forEach(label => {
+
+let color = ""
+
+if(label === "bug"){
+color = "bg-red-100 text-red-600"
+}
+
+else if(label === "help wanted"){
+color = "bg-yellow-100 text-yellow-700"
+}
+
+else if(label === "enhancement"){
+color = "bg-green-100 text-green-600"
+}
+
+else if(label === "documentation"){
+color = "bg-orange-100 text-orange-600"
+}
+
+else if(label === "good first issue"){
+color = "bg-blue-100 text-blue-600"
+}
+
+labels.innerHTML +=
+`<span class="${color} px-2 py-1 rounded text-xs font-bold">
+${label.toUpperCase()}
+</span>`
+
+})
+
+
+
+const modal = document.getElementById("issueModal")
+
+modal.classList.remove("hidden")
+modal.classList.add("flex")
+
+})
 
 }
 
 
 
-if (document.getElementById("issueGrid")) {
-    renderIssues(allIssuesData)
-}
 
-if (document.getElementById("issueGrid")) {
-    renderIssues(allIssuesData)
-}
+function closeModal(){
 
+const modal = document.getElementById("issueModal")
 
-
-// SEARCH FUNCTION
-function searchIssues() {
-
-    const keyword = document.getElementById("searchInput").value.toLowerCase()
-
-    const filtered = allIssuesData.filter(issue =>
-        issue.title.toLowerCase().includes(keyword)
-    )
-
-    renderIssues(filtered)
+modal.classList.add("hidden")
+modal.classList.remove("flex")
 
 }
+
+
+
+
+function searchIssues(){
+
+const keyword = document.getElementById("searchInput").value
+
+fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${keyword}`)
+.then(res => res.json())
+.then(data => {
+
+renderIssues(data.data)
+
+})
+
+}
+
 
 
 const searchInput = document.getElementById("searchInput")
 
-if (searchInput) {
+if(searchInput){
 
-    searchInput.addEventListener("keyup", function (e) {
+searchInput.addEventListener("keyup",function(e){
 
-        if (e.key === "Enter") {
-            searchIssues()
-        }
-
-    })
-
+if(e.key === "Enter"){
+searchIssues()
 }
 
-
-
-function searchIssues() {
-
-    let keyword = document.getElementById("searchInput").value.toLowerCase()
-
-    let filtered = allIssuesData.filter(function (issue) {
-
-        return issue.title.toLowerCase().includes(keyword)
-
-    })
-
-    renderIssues(filtered)
+})
 
 }
